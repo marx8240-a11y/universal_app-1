cb9422f9525aac621f73e8bc6eef9ce9e0ed3b6b
godson <nkansahgodson7@gmail.com>
2026-09-08 23:41:21 +0000

    Added Auth. Done for the day

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter ,HTTPException , status
 from sqlmodel import select
 from app.core.database import sessionDep
 from app.models.users import UserDb
-from app.schemas.users import UserPublic , UserCreate
-from app.core.security import hash_password
+from app.schemas.users import UserPublic , UserCreate , UserLogin , Token
+from app.core.security import hash_password , verify_password , create_access_token 
 
 router = APIRouter()
 
@@ -13,7 +13,7 @@ def create_user(session :sessionDep , user_data :UserCreate) ->UserDb:
     existing_user = session.exec(select(
         UserDb).where(UserDb.username == user_data.username)
         ).first()
-    
+
 
     if existing_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User Already exits!")
@@ -25,3 +25,13 @@ def create_user(session :sessionDep , user_data :UserCreate) ->UserDb:
     session.commit()
     session.refresh(user_db)
     return user_db
+
+
+@router.post("/login" , response_model=Token)
+def login(session :sessionDep ,user_data :UserLogin) -> Token:
+    existing_user = session.exec(select(UserDb).where(UserDb.username == user_data.username)).first()
+
+    if not existing_user or not verify_password(user_data.password , existing_user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED , detail="Invalid Username Or Password!")
+
+    return Token(access_token=create_access_token(existing_user.username))
